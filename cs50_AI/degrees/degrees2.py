8259f6ef426e7f2ca583015962fb60517af656d7
Tajgero <jasiek.latt@gmail.com>
2025-06-15 01:50:19 +0200

    updated tic tac toe

--- cs50_AI/degrees/degrees2.py
+++ cs50_AI/degrees/degrees2.py
@@ -87,7 +87,7 @@ def main():
 def shortest_path(source: str, target: str) -> list[tuple]:
     """
     Returns the shortest list of (movie_id, person_id) pairs
-    that connect the source to the target. (BFS algorithm)
+    that connect the source to the target. (Bidirectional BFS algorithm)
 
     If no possible path, returns None.
     
@@ -95,62 +95,91 @@ def shortest_path(source: str, target: str) -> list[tuple]:
     actions = movies
     
     """
-    # Frontier will be QueueFrontier -> FIFO, Breadth-First Search
-    frontier = QueueFrontier()
+    # Frontier will be QueueFrontier -> FIFO, Bidirectional Breadth-First Search
+    frontier_source = QueueFrontier()
+    frontier_target = QueueFrontier()
 
     # Initialize an empty explored set
-    explored_set = set()
+    explored_source = set()
+    explored_target = set()
     
     # Initialize frontier to just the starting position
     start = Node(state=source, parent=None, action=None)
+    end = Node(state=target, parent=None, action=None)
 
     # Add first node to frontier
-    frontier.add(start)
+    frontier_source.add(start)
+    frontier_target.add(end)
 
     # 1. Check until empty to find shortest path
-    while not frontier.empty():
-        
+    while not frontier_source.empty() and not frontier_target.empty():
+# =============================================================================
+#       FROM SOURCE
+# =============================================================================
         # 2. Remove from frontier and consider it
-        node = frontier.remove()  
+        node_source = frontier_source.remove()  
+        node_target = frontier_target.remove()  
         
-        # 3. Else expand node from this node and add to frontier and explored states
-        
-        # Mark node as explored
-        explored_set.add(node.state) 
+        # 3. Expand node from this node and add to frontier and explored states
+        explored_source.add(node_source.state) 
+        explored_target.add(node_target.state) 
         
         # Add neighbors to frontier
-        for action, state in neighbors_for_person(node.state):
-            if state not in explored_set and not frontier.contains_state(state):
+        for action, state in neighbors_for_person(node_source.state):
+            if state not in explored_source and not frontier_source.contains_state(state):
                 # next person in neighbors, parent person, movie starred in
-                child = Node(state=state, parent=node, action=action)
+                child = Node(state=state, parent=node_source, action=action)
                 
-        # 4. If found target, return solution
-                if child.state == target:
-                    return solution_path(child)
+                # 4a. If found in explored target set, merge paths
+                if state in explored_target:
+                    
+                    # Source, Target
+                    return merge_paths(child, node_target)
 
-                frontier.add(child)
+                frontier_source.add(child)
+# =============================================================================
+#       FROM TARGET    
+# =============================================================================
+        # Add neighbors to frontier
+        for action, state in neighbors_for_person(node_target.state):
+            if state not in explored_target and not frontier_target.contains_state(state):
+                # next person in neighbors, parent person, movie starred in
+                child = Node(state=state, parent=node_target, action=action)
                 
+                # 4b. If found in explored source set, merge paths
+                if state in explored_source:
+                    
+                    # Source, Target
+                    return merge_paths(node_source, child)
+
+                frontier_target.add(child)
+    
     return None
                 
                 
-def solution_path(node):
+def merge_paths(source_node, target_node):
     """
-    Returns solution if found for node
+    Returns solution if found from source or from target and merge paths
     """
-
-    solution = [] # (movie_id -> actions, person_id -> states)
+    path_forward = []
+    path_backward = []
     
-    # Recursive nodes search backwards from target to source
-    while node.parent is not None:
+    # Iterate through parent nodes
+    while source_node.parent is not None:
         
-        # Append tuples
-        solution.append((node.action, node.state))
-        node = node.parent
-        
-        # Reorder list with actions and states from source to target
-        solution.reverse()
-        
-    return solution
+        # Tuple -> (action, state)        
+        path_forward.append((source_node.action, source_node.state))
+        source_node = source_node.parent
+    
+    path_forward.reverse()
+    # Again but backwards
+    while target_node.parent is not None:
+         
+         path_backward.append((target_node.action, target_node.state))
+         target_node = target_node.parent
+         
+    return path_forward + path_backward
+    
 
 def person_id_for_name(name):
     """
--- cs50_AI/tictactoe/tictactoe.py
+++ cs50_AI/tictactoe/tictactoe.py
@@ -4,6 +4,7 @@ Tic Tac Toe Player
 
 import math
 from copy import deepcopy
+from random import shuffle
 
 X = "X"
 O = "O"
@@ -25,11 +26,11 @@ def player(board):
     """
     x_counter = 0
     o_counter = 0
-    for row in board:
-        for col in row:
-            if board[row][col] == X:
+    for i, row in enumerate(board):
+        for j, col in enumerate(row):
+            if board[i][j] == X:
                 x_counter += 1
-            elif board[row][col] == O:
+            elif board[i][j] == O:
                 o_counter += 1
     
     if x_counter > o_counter:
@@ -43,12 +44,14 @@ def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    actions = set()
+    actions = list()
     for i, row in enumerate(board):
         for j, col in enumerate(row):
             if board[i][j] == EMPTY:
-                actions.add((i, j))
+                actions.append((i, j))
                 
+    shuffle(actions) # More random actions
+    
     # Return indexes of a board with EMPTY fields
     return actions
 
@@ -61,46 +64,90 @@ def result(board, action):
         raise ValueError("Invalid action")
     
     # Create deepcopy of board object to retain AI mechanics
-    board = deepcopy(board)
+    new_board = deepcopy(board)
     
     i, j = action
     
-    # Return new board (i, j of action) based on player action (X or O)
-    board[i][j] = player(action)
+    # Return new board (i, j of action) based on player state (X or O)
+    new_board[i][j] = player(new_board)
+    
+    return new_board
 
 
-def check_col(board):
-    pass# TODO
+def check_row_col(board, player) -> bool:
+    """
+    For winner function to check if row
+    or column is complete and return symbol.
+    """
+    # Board is square so ...
+    for i in range(len(board)):
+        
+        # Checks if any row have exactly same symbol
+        if board[i][0] == board[i][1] == board[i][2] == player:
+            return True
+        
+        # Checks if any column have exactly same symbol
+        elif board[0][i] == board[1][i] == board[2][i] == player:
+            return True
+        
+    return False
+            
 
-def check_row(board):
-    pass# TODO
-
-def check_diag(board):
-    pass# TODO
+def check_diag(board, player) -> bool:
+    """
+    For winner function to check if diagonal is complete and return symbol.
+    
+    Board indices:
+   
+   (0,0)| 0,1 |(0,2)
+    ____|_____|____
+        |     |               Diagonal win if list[0] or list[1]:
+    1,0 |(1,1)| 1,2    ===>  [[(0,0), (1,1), (2,2)],
+    ____|_____|____           [(2,0), (1,1), (0,2)]]
+        |     |
+   (2,0)| 2,1 |(2,2)
+    """
+            
+    if board[0][0] == board[1][1] == board[2][2] == player:
+        return True
+    
+    elif board[2][0] == board[1][1] == board[0][2] == player:
+        return True
+    
+    return False
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    if check_col or check_row or check_diag:
+    if check_row_col(board, X) or check_diag(board, X):
         return X
+    elif check_row_col(board, O) or check_diag(board, O):
         return O
     else:
         return None
-    # TODO
-    for i, row in enumerate(board):
-        for j, col in enumerate(row):
-            pass
 
 
-def terminal(board):
+def fast_win(board, player):
+    """
+    If AI can win instantly in next move, return action or False otherwise
+    """
+    for action in actions(board):
+        board_next_move = result(board, action)
+        if check_diag(board_next_move, player) or check_row_col(board_next_move, player):
+            return action
+    
+    return False
+
+
+def terminal(board) -> bool:
     """
     Returns True if game is over, False otherwise.
     """
     
-    # 1. If there is a winner    
-    if winner(board) == X or winner(board) == O:
+    # 1. If there is a winner no matter which    
+    if winner(board):
         return True
     
     # 2. If all spaces are empty, game still plays
@@ -112,7 +159,7 @@ def terminal(board):
     return True
 
 
-def utility(board):
+def utility(board) -> int:
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
@@ -123,18 +170,93 @@ def utility(board):
     else:
         return 0
     
-    for i, row in enumerate(board):
-        for j, col in enumerate(row):
-            pass
 
-
-def minimax(board):
+def minimax(board: list[list, list, list]) -> tuple:
     """
     Returns the optimal action for the current player on the board.
+    
+    X - maximizer
+    O - minimizer
+    player(board) - X or O turn
+    actions(board) - set of actions to make
+    utility(board) - when terminal state: X = 1, O = -1, Tie = 0
+    result(board, action) - new board based on state, action and
+                            player turn already taken in this func
     """
-
-    # TODO
-    for i, row in enumerate(board):
-        for j, col in enumerate(row):
-            pass
+    # Main board which will split into different result boards recursively depending
+    # on player X or O
+    
+    
+    # 1. If game over, then AI stops
+    if terminal(board) == True:
+        return None
         
+    # 2. Recursive algorithm in functions minValue, maxValue independently
+    # In this place, returning value must have some optimal moves and best
+    # possible outcome of a game
+    elif player(board) == X: # MAX player
+        best_value, best_action = maxValue(board)
+        
+        # 2a. If in next AI will win, then return that action
+        if action := fast_win(board, player=O):
+            return action
+        
+    elif player(board) == O: # MIN player
+        
+        # 2b. If in next AI will win, then return that action
+        if action := fast_win(board, player=O):
+            return action
+        
+        best_value, best_action = minValue(board)
+    
+    return best_action
+        
+    # TODO -> A,B Pruning
+    
+def minValue(board) -> tuple:
+    """
+    Recursive reasoning:
+    Returns value 1, 0 or -1 for minimizing possible terminal states of a game
+    taken by player, who is trying to minimize outcome of this game
+    """
+    # Base case for maxValue function -> stops when recursion is complete it
+    if terminal(board):
+        return utility(board), None # None is essential since I can now call tuple as [0] index
+    
+    v = math.inf # Actions always better than this (for the first time)
+    
+    # Recursive alternating max and min functions
+    for action in actions(board):
+        v_new = min(v, maxValue(result(board, action))[0])
+        
+        # Keep track of best move and replacing utility return number
+        if v_new < v:
+            v = v_new
+            best_move = action
+
+    return v_new, best_move
+
+
+def maxValue(board) -> tuple:
+    """
+    Recursive reasoning:
+    Returns value 1, 0 or -1 for maximizing possible terminal states of a game
+    taken by player, who is trying to minimize outcome of this game
+    """
+    # Base case for minValue function
+    if terminal(board):
+        return utility(board), None # None is essential since I can now call tuple as [0] index
+    
+    v = -math.inf # Actions always better than this (for the first time) -> stops when recursion is complete it
+    
+    # Recursive alternating max and min functions
+    for action in actions(board):
+        v_new = max(v, minValue(result(board, action))[0])
+        
+        # Keep track of best move and replacing utility return number
+        if v_new > v:
+            v = v_new
+            best_move = action
+  
+    return v_new, best_move
+
