46263db071f53a7160450c8d043aa989e7cfd1e4
Tajgero <jasiek.latt@gmail.com>
2025-06-18 19:13:12 +0200

    Added Sloth Challenges

--- SlothBytes_challenges/birthday_cake_candles.py
+++ SlothBytes_challenges/birthday_cake_candles.py
@@ -1,20 +1,45 @@
-def birthdayCakeCandles(candles: list) -> int:
-    """Returns number of tallest candles on a cake"""
+class Candles():
     
-    if len(candles) == 0:
-        print("output = 0")
-        return 0
+    def __init__(self, input):
+        self.count, self.tallest = self.birthdayCakeCandles(input)
+        print(self)
+        
+    def __repr__(self):
+        return f"Count: {self.count}\nTallest: {self.tallest}"
     
-    tallest = max(candles)
-    n = 0
-    for candle in candles:
-        if candle == tallest:
-            n += 1
-    
-    print(f"output = {n}")
-    return n
+    def birthdayCakeCandles(self, candles: list) -> tuple:
+        """
+        Returns count and tallest of candles on a cake
+        in more complicated recursive way than just
+        5 lines of code
+        """
+        this_candle = 0
+        
+        # Base Case
+        try:
+            this_candle = candles[0]
+        except:
+            # When recursion ends returns count and candle height
+            return 0, this_candle
+        
+        # Recursion
+        n, next_candle = self.birthdayCakeCandles(candles[1:])
+        
+        # Checking candle heights
+        if next_candle < this_candle:
+            n = 1
+            
+        elif next_candle > this_candle:
+            this_candle = next_candle
+            
+        elif next_candle == this_candle:
+            return 1 + n, this_candle
+           
+            
+        return n, this_candle
+        
 
-
-birthdayCakeCandles([4,4,1,3])
-birthdayCakeCandles([1, 1, 1, 1])
-birthdayCakeCandles([])
+Candles([4,4,1,3])
+Candles([4,4,1,3])
+Candles([1, 1, 1, 1])
+Candles([])
